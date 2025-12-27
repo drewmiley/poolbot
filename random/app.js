dcb14f832ad70413852be292ad9e635221558a5b
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2025-12-27 00:21:13 +0000

    Implements random generator

---
+++ random/app.js
@@ -0,0 +1,14 @@
+document.getElementById('run').onclick = () => run();
+
+const getSelectedValue = (id) => {
+	const e = document.getElementById(id);
+	return e.value;
+}
+
+const run = () => {
+    console.log('Running');
+    const max = parseInt(getSelectedValue('number'));
+    const result = Math.ceil(Math.random() * max);
+	document.getElementById('result').innerText = result;
+	console.log('Done');
+}
