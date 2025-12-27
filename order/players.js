d95ac9eee495839672002ba8249bcba4bdca68a1
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2025-12-27 20:30:19 +0000

    Adds in player information

--- order/players.js
+++ order/players.js
@@ -1,21 +1,45 @@
-const player = (initial, preferenceBeforeInit, wouldPreferToRef) => {
+const EARLY = "early";
+const MIDDLE = "middle";
+const LATE = "late";
+
+const getFrameOptions = (preferences, isSecondHalf) => {
+	// TODO: Clean up this function
+	let arr = [];
+	if (preferences.includes(EARLY)) {
+		arr.push([0, 1]);
+	}
+	if (preferences.includes(MIDDLE)) {
+		arr.push([2, 3]);
+	}
+	if (preferences.includes(LATE)) {
+		isSecondHalf ? arr.push([4, 5]) : arr.push([4]);
+	}
+	return arr;
+}
+
+const getFirstHalfOptions = firstHalfPreferences => getFrameOptions(firstHalfPreferences, false);
+
+const getSecondHalfOptions = secondHalfPreferences => getFrameOptions(secondHalfPreferences, false);
+
+const player = (initial, breakPreferences, firstHalfPreferences, secondHalfPreferences) => {
+	const numbersOfBreaks = breakPreferences.reduce((acc, val) => acc + val) / breakPreferences.length;
+	const firstHalfOptions = getFirstHalfOptions(firstHalfPreferences);
+	const secondHalfOptions = getSecondHalfOptions(secondHalfPreferences);
 	return {
-		initial,
-		preferenceBefore: preferenceBeforeInit === null ? Math.random() < 0.5 : preferenceBeforeInit,
-		wouldPreferToRef
+		numbersOfBreaks,
+		firstHalfOptions,
+		secondHalfOptions
 	}
 }
 
-// TODO: Write up data model for players from votes. Number of breaks preference, first half preference, second half preference
-
 const players = () => [
-	player('CC', null),
-	player('DD', false),
-	player('DM', false, true),
-	player('DW', false),
-	player('JC', true),
-	player('ND', true, true),
-	player('PC', true)
+	player('CC', [1], [EARLY, MIDDLE], [EARLY, MIDDLE]),
+	player('DD', [1], [MIDDLE, LATE], [EARLY, MIDDLE]),
+	player('DM', [0, 1], [MIDDLE, LATE], [MIDDLE, LATE]),
+	player('DW', [1], [EARLY, MIDDLE, LATE], [EARLY, MIDDLE, LATE]),
+	player('JC', [0, 1], [MIDDLE], [EARLY]),
+	player('ND', [2], [MIDDLE, LATE], [MIDDLE, LATE]),
+	player('PC', [1], [EARLY, MIDDLE], [EARLY, MIDDLE, LATE])
 ]
 
 console.log(players())
