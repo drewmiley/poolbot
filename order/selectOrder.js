1972c14a4de9464137010679d06a3faf18f32f6b
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2025-12-28 01:14:30 +0000

    Addresses most tidy up TODOs

--- order/selectOrder.js
+++ order/selectOrder.js
@@ -1,29 +1,55 @@
-const getBreaksAssigned = (players, frames) => {
-	// TODO: Tidy up this function - give better name to function
-	let playersWithWeighting = players.reduce((acc, player) => {
+const getHalfPlayers = (players, options) => {
+	const halfPlayers = players
+		.filter(player => options[player.initial.toLowerCase()].isSelected)
+		.map(player => {
+			const numberOfBreaks = player.numberOfBreaks - (options.isSecondHalf && options[player.initial.toLowerCase()].hadBreak ? 1 : 0);
+			return {
+				initial: player.initial,
+				frameOptions: options.isSecondHalf ? player.secondHalfOptions : player.firstHalfOptions,
+				numberOfBreaks: Math.max(numberOfBreaks, 0)
+			};
+		});
+
+	const reserves = [...Array(options.numberOfReserves).keys()].map(index => {
+		return {
+			initial: `R${i + 1}`,
+			frameOptions: options.isSecondHalf ? [0, 1, 2, 3, 4] : [0, 1, 2, 3, 4, 5],
+			numberOfBreaks: 0.5
+		}
+	})
+
+	return halfPlayers.concat(reserves);
+}
+
+const getPlayersAssignedBreaks = (players, frames) => {
+	const playersWithWeighting = players.reduce((acc, player) => {
 		const playerArray = [...Array(player.numberOfBreaks * 2).keys()].map(_ => player.initial);
 		return acc.concat(playerArray)
 	}, []);
-	const numberOfBreaks = frames.filter(frame => frame).length;
+	const numberOfBreaks = frames.filter(hasBreak => hasBreak).length;
 	let breaks = [];
+	// TODO: Can I remove while loop here?
 	while (breaks.length < numberOfBreaks) {
-		let index = Math.floor(Math.random() * playersWithWeighting.length);
-		let breakingPlayer = playersWithWeighting[index];
-		breaks.push(breakingPlayer);
-		playersWithWeighting = playersWithWeighting.filter(player => players != breakingPlayer);
+		const remainingPlayersWithWeighting = playersWithWeighting.filter(player => !breaks.includes(player));
+		const index = Math.floor(Math.random() * remainingPlayersWithWeighting.length);
+		breaks.push(remainingPlayersWithWeighting[index]);
 	}
 	return breaks;
 };
 
-const getFramePermutationWithScore = (framePermutation, playersWithBreaksAssigned, isSecondHalf) => {
-	// TODO: Tidy up this function
-	const numberOfFrames = isSecondHalf ? 5 : 6;
-	let score = 0;
-	for (let i = 0; i < numberOfFrames; i++) {
+const getBestFramePermutations = (framePermutation, playersWithBreaksAssigned) => {
+	let unhappyFrames = 0;
+	for (let i = 0; i < playersWithBreaksAssigned.length; i++) {
 		const frameIsCorrect = playersWithBreaksAssigned.find(player => player.initial == framePermutation[i]).frameOptions.includes(i);
-		score += frameIsCorrect ? 1 : 0;
+		unhappyFrames += !frameIsCorrect ? 1 : 0;
 	}
-	return numberOfFrames - 1 <= score ? { framePermutation, score, perfect: numberOfFrames === score } : null;
+	return unhappyFrames <= 1 ? { framePermutation, perfect: !unhappyFrames } : null;
+}
+
+const selectPermutation = framePermutationsWithScore => {
+	const perfectPermutations = framePermutationsWithScore.filter(perm => perm.perfect);
+	const index = perfectPermutations.length ? Math.floor(Math.random() * perfectPermutations.length) : Math.floor(Math.random() * framePermutationsWithScore.length)
+	return perfectPermutations.length ? perfectPermutations[index].framePermutation : framePermutationsWithScore[index].framePermutation;
 }
 
 function clearText() {
@@ -43,29 +69,7 @@ function selectOrder(options, players) {
 	console.log(options);
 	console.log(players);
 
-	// TODO: Tidy up this function
-	const halfPlayers = players
-		.filter(player => {
-			return options[player.initial.toLowerCase()].isSelected;
-		})
-		.map(player => {
-			const numberOfBreaks = player.numberOfBreaks - (options.isSecondHalf && options[player.initial.toLowerCase()].hadBreak ? 1 : 0);
-			return {
-				initial: player.initial,
-				frameOptions: options.isSecondHalf ? player.secondHalfOptions : player.firstHalfOptions,
-				numberOfBreaks: Math.max(numberOfBreaks, 0)
-			};
-		});
-
-	// TODO: Neater way of doing this
-	for (let i = 0; i < options.numberOfReserves; i++) {
-		let reserve = {
-			initial: `R${i + 1}`,
-			frameOptions: options.isSecondHalf ? [0, 1, 2, 3, 4] : [0, 1, 2, 3, 4, 5],
-			numberOfBreaks: 0.5
-		}
-		halfPlayers.push(reserve);
-	}
+	const halfPlayers = getHalfPlayers(players, options);
 
 	if (halfPlayers.length != (options.isSecondHalf ? 5 : 6)) {
 		document.getElementById('errorText').innerText = 'Wrong number of players selected - please modify options';
@@ -87,10 +91,10 @@ function selectOrder(options, players) {
 	console.log(halfPlayers);
 	console.log(framesInHalf);
 
-	const breaksAssigned = getBreaksAssigned(halfPlayers, framesInHalf);
+	const playersAssignedBreaks = getPlayersAssignedBreaks(halfPlayers, framesInHalf);
 
 	const playersWithBreaksAssigned = halfPlayers.map(player => {
-		const playerHasBreak = breaksAssigned.includes(player.initial);
+		const playerHasBreak = playersAssignedBreaks.includes(player.initial);
 		return {
 			initial: player.initial,
 			frameOptions: player.frameOptions.filter(frameIndex => framesInHalf[frameIndex] === playerHasBreak)
@@ -101,32 +105,28 @@ function selectOrder(options, players) {
 
 	const allPossibleFramePermuations = permutations(playersWithBreaksAssigned.map(player => player.initial));
 
-	const framePermutationsWithScore = allPossibleFramePermuations
-		.map(framePermuation => getFramePermutationWithScore(framePermuation, playersWithBreaksAssigned, options.isSecondHalf))
+	const bestFramePermutations = allPossibleFramePermuations
+		.map(framePermuation => getBestFramePermutations(framePermuation, playersWithBreaksAssigned))
 		.filter(framePermuation => framePermuation != null);
 
 	console.log(allPossibleFramePermuations.length);
 
-	console.log(framePermutationsWithScore);
+	console.log(bestFramePermutations);
 
-	if (!framePermutationsWithScore.length) {
+	if (!bestFramePermutations.length) {
 		document.getElementById('errorText').innerText = 'No order found - please re-run to find another';
 		return;
 	}
 
-	// TODO: Maybe draw this out as function
-	const perfectPermutations = framePermutationsWithScore.filter(perm => perm.perfect);
+	const selectedPermutation = selectPermutation(bestFramePermutations);
 
-	const index = perfectPermutations.length ? Math.floor(Math.random() * perfectPermutations.length) : Math.floor(Math.random() * framePermutationsWithScore.length)
-	const selectedPermuation = perfectPermutations.length ? perfectPermutations[index].framePermutation : framePermutationsWithScore[index].framePermutation;
-
-	document.getElementById('orderOne').innerText = `${selectedPermuation[0]}${framesInHalf[0] ? ' (B)' : ''}`;
-	document.getElementById('orderTwo').innerText = `${selectedPermuation[1]}${framesInHalf[1] ? ' (B)' : ''}`;
-	document.getElementById('orderThree').innerText = `${selectedPermuation[2]}${framesInHalf[2] ? ' (B)' : ''}`;
-	document.getElementById('orderFour').innerText = `${selectedPermuation[3]}${framesInHalf[3] ? ' (B)' : ''}`;
-	document.getElementById('orderFive').innerText = `${selectedPermuation[4]}${framesInHalf[4] ? ' (B)' : ''}`;
+	document.getElementById('orderOne').innerText = `${selectedPermutation[0]}${framesInHalf[0] ? ' (B)' : ''}`;
+	document.getElementById('orderTwo').innerText = `${selectedPermutation[1]}${framesInHalf[1] ? ' (B)' : ''}`;
+	document.getElementById('orderThree').innerText = `${selectedPermutation[2]}${framesInHalf[2] ? ' (B)' : ''}`;
+	document.getElementById('orderFour').innerText = `${selectedPermutation[3]}${framesInHalf[3] ? ' (B)' : ''}`;
+	document.getElementById('orderFive').innerText = `${selectedPermutation[4]}${framesInHalf[4] ? ' (B)' : ''}`;
 	if (!options.isSecondHalf) {
-		document.getElementById('orderSix').innerText = `${selectedPermuation[5]}${framesInHalf[5] ? ' (B)' : ''}`;
+		document.getElementById('orderSix').innerText = `${selectedPermutation[5]}${framesInHalf[5] ? ' (B)' : ''}`;
 	}
 	console.log('Done');
 }
