8cd59e65b124469dad041733b8881f8af2615dfb
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2026-01-30 18:04:02 +0000

    Iterates over players to set order options

--- order/app.js
+++ order/app.js
@@ -19,19 +19,32 @@ const getSelectedOrderParams = (id) => {
 
 const run = () => {
     console.log('Running');
-    // TODO: Add programmatic setting of players, inc. gp/GP. Probably using spread operator.
-    const options = {
+    // TODO: Add programmatic setting of players, inc. gp/GP.
+    // TODO: Can i refactor this, probably using spread operator.
+    const team = players();
+    const teamInitials = team.map(player => player.initial);
+    const initialOptions = {
         teamAreAway: getSelectedCheckbox('teamAreAway'),
         isSecondHalf: getSelectedCheckbox('isSecondHalf'),
-    	cc: getSelectedOrderParams('cc'),
-        dd: getSelectedOrderParams('dd'),
-        dm: getSelectedOrderParams('dm'),
-        dw: getSelectedOrderParams('dw'),
-        jc: getSelectedOrderParams('jc'),
-        nd: getSelectedOrderParams('nd'),
-        pc: getSelectedOrderParams('pc'),
-    	numberOfReserves: parseInt(getSelectedValue('numberOfReserves'))
+        numberOfReserves: parseInt(getSelectedValue('numberOfReserves'))
     }
-    const team = players();
+    const options = teamInitials.reduce((accOptions, playerInitial) => {
+        const playerInitialToLowerCase = playerInitial.toLowerCase();
+        accOptions[playerInitialToLowerCase] = getSelectedOrderParams(playerInitialToLowerCase);
+        return accOptions;
+    }, initialOptions);
+    // const options = {
+    //     teamAreAway: getSelectedCheckbox('teamAreAway'),
+    //     isSecondHalf: getSelectedCheckbox('isSecondHalf'),
+    // 	cc: getSelectedOrderParams('cc'),
+    //     dd: getSelectedOrderParams('dd'),
+    //     dm: getSelectedOrderParams('dm'),
+    //     dw: getSelectedOrderParams('dw'),
+    //     jc: getSelectedOrderParams('jc'),
+    //     nd: getSelectedOrderParams('nd'),
+    //     pc: getSelectedOrderParams('pc'),
+    // 	numberOfReserves: parseInt(getSelectedValue('numberOfReserves'))
+    // }
+    // const team = players();
     selectOrder(options, team);
 }
